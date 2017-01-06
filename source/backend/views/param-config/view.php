19d4c0f4ae136b57542ef38bce8af2281aa073d6
toanhv <toanhv.itvn@gmail.com>
2017-01-07 00:13:28 +0700

    d

--- source/backend/views/param-config/view.php
+++ source/backend/views/param-config/view.php
@@ -26,34 +26,42 @@ $this->params['breadcrumbs'][] = $this->title;
             'module.name',
             [
                 'label' => 'Convection Pump',
+                'format' => 'html',
                 'value' => $model->getConvectionTemp(),
             ],
             [
                 'label' => 'Cold Water Supply Pump',
+                'format' => 'html',
                 'value' => 'Water level M1: ' . $model->getCwsplv1() . ' - Water level M2: ' . $model->getCwsplv2(),
             ],
             [
                 'label' => 'Return Pump',
+                'format' => 'html',
                 'value' => 'Begin ilde time: ' . $model->getReturnPumpBegin() . '<br>End ilde time: ' . $model->getReturnPumpEnd() . '<br>Rang to turn on the pump: ' . $model->getReturnPumpDeltat(),
             ],
             [
                 'label' => 'Incresed Pressure Pump',
+                'format' => 'html',
                 'value' => 'Pressure to turn on the pump: ' . $model->getPressurePumpP1(),
             ],
             [
                 'label' => 'Heat Pump',
+                'format' => 'html',
                 'value' => 'Temperature to turn on the pump: ' . $model->getHeatPumpT1(),
             ],
             [
                 'label' => 'Heat Resistor',
+                'format' => 'html',
                 'value' => 'Temperature to turn on the Resistor: ' . $model->getHeaterResisT1() . '<br>Delay time to return on Resistor: ' . $model->getHeaterResisDelay(),
             ],
             [
                 'label' => 'Three Way Valve',
+                'format' => 'html',
                 'value' => 'Begin time ilde: ' . $model->get3wayBeginTime() . '<br>End time ilde: ' . $model->get3wayEndTime() . '<br>Rang to change direction: ' . $model->get3wayTempDelta(),
             ],
             [
                 'label' => 'Backflow Valve',
+                'format' => 'html',
                 'value' => 'Temperature value to open Valve: ' . $model->getBackflowTemp(),
             ],
         ],
