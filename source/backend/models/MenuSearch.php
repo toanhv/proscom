253cf189ad8293043074302afc53b55d4d14c845
toanhv <toanhv.itvn@gmail.com>
2017-01-08 01:01:53 +0700

    sdf

--- source/backend/models/MenuSearch.php
+++ source/backend/models/MenuSearch.php
@@ -56,7 +56,7 @@ class MenuSearch extends Menu {
             'desc' => ['parent.order' => SORT_DESC, 't.order' => SORT_DESC],
             'label' => 'order',
         ];
-        $sort->defaultOrder = ['order' => SORT_ASC];
+        $sort->defaultOrder = ['parent.order' => SORT_ASC];
 
         if (!($this->load($params) && $this->validate())) {
             return $dataProvider;
