ad7b60380ac7e6d523e4c3bb8f51f130f0ea7662
toanhv <toanhv.itvn@gmail.com>
2017-01-13 21:32:13 +0700

    adf

--- source/common/config/main-local.php
+++ source/common/config/main-local.php
@@ -4,9 +4,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=proscom',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=124.158.12.89;dbname=proscom',
+            'username' => 'proscom',
+            'password' => 'proscom@123312##',
             'charset' => 'utf8',
             'enableSchemaCache' => true,
             'schemaCacheDuration' => 3600,
