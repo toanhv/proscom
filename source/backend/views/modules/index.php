adb294891140c14e3c4622032e885c688472cc4f
toanhv <toanhv.itvn@gmail.com>
2017-01-21 22:36:39 +0700

    df

--- source/backend/views/modules/index.php
+++ source/backend/views/modules/index.php
@@ -69,10 +69,10 @@ $this->title = 'Home';
             ?>
             <div class="col-md-4">
                 <a href="<?php echo $url; ?>" title="<?php echo \yii\helpers\Html::encode($val->name); ?>">
-                    <img class="img-responsive" src="<?php echo $img; ?>" alt="<?php echo \yii\helpers\Html::encode($val->name); ?>" />
-                    <p class="">ID: <?php echo \yii\helpers\Html::encode($val->getModuleId()); ?></p>
-                    <p class=""><?php echo \yii\helpers\Html::encode($val->name); ?></p>
+                    <img class="img-responsive" src="<?php echo $img; ?>" alt="<?php echo \yii\helpers\Html::encode($val->name); ?>" />                    
                 </a>
+                <p class="">ID: <?php echo \yii\helpers\Html::encode($val->getModuleId()); ?></p>
+                <p class=""><?php echo \yii\helpers\Html::encode($val->name); ?></p>
             </div>
             <?php
         }
