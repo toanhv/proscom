d5785afbfcad726bb08117c0f1ccba6599ae0d9b
toanhv <toanhv.itvn@gmail.com>
2017-10-24 23:40:29 +0700

    sdf

--- Freebitcoin/auto_roll.bat
+++ Freebitcoin/auto_roll.bat
@@ -1,13 +1,13 @@
 @echo off
 taskkill /f /t /im Firefox.exe /im crashreporter.exe >nul 2>&1
-timeout 10
-start firefox -no-remote imacros://run/?m=recaptcha.iim
+timeout 20
+start firefox -no-remote -P "toanhv18" imacros://run/?m=recaptcha.iim
 timeout 600
 taskkill /f /t /im Firefox.exe /im crashreporter.exe >nul 2>&1
-start firefox -no-remote imacros://run/?m=captcha.iim
+start firefox -no-remote -P "toanhv18" imacros://run/?m=captcha.iim
 timeout 600
 taskkill /f /t /im Firefox.exe /im crashreporter.exe >nul 2>&1
-start firefox -no-remote imacros://run/?m=doge.iim
+start firefox -no-remote -P "doge.toanhv10" imacros://run/?m=doge.iim
 timeout 600
 taskkill /f /t /im Firefox.exe /im crashreporter.exe >nul 2>&1
 %0
