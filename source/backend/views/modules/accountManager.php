aa50007fe7b37acbfca70d8caa63fac337172b52
toanhv <toanhv.itvn@gmail.com>
2017-11-12 00:23:02 +0700

    sdf

--- source/backend/views/modules/accountManager.php
+++ source/backend/views/modules/accountManager.php
@@ -9,83 +9,83 @@
                 <!--<button class="link"  onclick="$('#manager-form').submit()">Check <br>Account</button>-->
                 <button class="link" onclick="javascript:void(0);">Balance</button>
                 <div class="content">
-                    <div class="text-02">Money: <strong id="money-info"><?php echo number_format($model->money) ?></strong> VND</div>
-                </div>
-            </div>
-            <div class="row-check-account">
-                <!--<button class="link" onclick="$('#manager-form').submit()" class="link">Check <br>Data</button>-->
-                <button class="link" onclick="javascript:void(0);" class="link">Data</button>
-                <div class="content">
-                    <div class="text-02">Data: <strong id="data-info"><?php echo number_format($model->data) ?></strong> KB</div>
-                </div>
-            </div>
-        </form>
-        <form action="/modules/accountmanager?id=<?php echo $model->id ?>" id="pay_card_form" method="post">
-            <input type="hidden" name="_csrf" value="<?php Yii::$app->request->csrfToken ?>">
-            <input type="hidden" name="pay" value="1">
-            <div class="row-check-account">
-                <button class="link" onclick="return checkCard()">SIM <br>Balance</button>
-                <div class="content">
-                    <div class="text-02">
-                        <input type="text" id="card_info" name="card_info" placeholder="Enter your code" class="text-field">
+                    <div class="text-02">Money: <strong id="money-info"><?php echo number_format($model->money) ?>
                     </div>
                 </div>
+                <div class="row-check-account">
+                    <!--<button class="link" onclick="$('#manager-form').submit()" class="link">Check <br>Data</button>-->
+                    <button class="link" onclick="javascript:void(0);" class="link">Data</button>
+                    <div class="content">
+                        <div class="text-02">Data: <strong id="data-info"><?php echo number_format($model->data) ?>
+                        </div>
+                    </div>
+                    </form>
+                    <form action="/modules/accountmanager?id=<?php echo $model->id ?>" id="pay_card_form" method="post">
+                        <input type="hidden" name="_csrf" value="<?php Yii::$app->request->csrfToken ?>">
+                        <input type="hidden" name="pay" value="1">
+                        <div class="row-check-account">
+                            <button class="link" onclick="return checkCard()">SIM <br>Balance</button>
+                            <div class="content">
+                                <div class="text-02">
+                                    <input type="text" id="card_info" name="card_info" placeholder="Enter your code" class="text-field">
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
             </div>
-        </form>
-    </div>
+            <?php if ($alert): ?>
+                <script type="text/javascript">
+                    alert('<?php echo $alert ?>')
+                </script>
+            <?php endif; ?>
+            <script type="text/javascript">
 
-</div>
-<?php if ($alert): ?>
-    <script type="text/javascript">
-        alert('<?php echo $alert ?>')
-    </script>
-<?php endif; ?>
-<script type="text/javascript">
+                function loadInfo() {
+                    console.log("reload module data");
+                    var id = $("#module-id").val();
 
-    function loadInfo() {
-        console.log("reload module data");
-        var id = $("#module-id").val();
+                    $.get("/modules/loadinfo?id=" + id, {}, function (values) {
+                        console.log(values);
+                        // var values = JSON.parse(data);
+                        $("#money-info").html(values.money);
+                        $("#data-info").html(values.data);
+                    });
 
-        $.get("/modules/loadinfo?id=" + id, {}, function (values) {
-            console.log(values);
-            // var values = JSON.parse(data);
-            $("#money-info").html(values.money);
-            $("#data-info").html(values.data);
-        });
+                }
 
-    }
+                setInterval(function () {
+                    console.log("auto reload module data");
+                    var id = $("#module-id").val();
 
-    setInterval(function () {
-        console.log("auto reload module data");
-        var id = $("#module-id").val();
+                    $.get("/modules/loadinfo?id=" + id, {}, function (values) {
+                        console.log(values);
+                        // var values = JSON.parse(data);
+                        $("#money-info").html(values.money);
+                        $("#data-info").html(values.data);
+                    });
 
-        $.get("/modules/loadinfo?id=" + id, {}, function (values) {
-            console.log(values);
-            // var values = JSON.parse(data);
-            $("#money-info").html(values.money);
-            $("#data-info").html(values.data);
-        });
+                }, 10000);
 
-    }, 10000);
+                function updateModuleInput() {
+                    $('#module-id').val($('#module_id').val());
+                    loadInfo();
+                }
 
-    function updateModuleInput() {
-        $('#module-id').val($('#module_id').val());
-        loadInfo();
-    }
-
-    function checkCard() {
-//        var card = $('#card_info').val();
-//        if (card.length < 12 || card.length > 16) {
-//            alert("Card code invalid!");
-//            $('#card_info').focus();
-//            return false;
-//        }
-//
-//        if (!$.isNumeric(card)) {
-//            alert("Card code invalid!");
-//            $('#card_info').focus();
-//            return false;
-//        }
-        $('#pay_card_form').submit();
-    }
-</script>
+                function checkCard() {
+                    //        var card = $('#card_info').val();
+                    //        if (card.length < 12 || card.length > 16) {
+                    //            alert("Card code invalid!");
+                    //            $('#card_info').focus();
+                    //            return false;
+                    //        }
+                    //
+                    //        if (!$.isNumeric(card)) {
+                    //            alert("Card code invalid!");
+                    //            $('#card_info').focus();
+                    //            return false;
+                    //        }
+                    $('#pay_card_form').submit();
+                }
+            </script>
