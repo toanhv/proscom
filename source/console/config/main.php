41ab9f027a95f1795208cc02879d1b87664d8f99
toanhv <toanhv.itvn@gmail.com>
2018-06-08 22:39:35 +0700

    asdf

--- source/console/config/main.php
+++ source/console/config/main.php
@@ -1,5 +1,6 @@
 <?php
 
+require(__DIR__ . '/../../common/config/predefined.php');
 $params = array_merge(
         require(__DIR__ . '/../../common/config/params.php'), require(__DIR__ . '/../../common/config/params-local.php'), require(__DIR__ . '/params.php'), require(__DIR__ . '/params-local.php')
 );
