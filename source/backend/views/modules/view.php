52ed68963c07581796eaccc40cb1e2d5b519e45b
toanhv <toanhv.itvn@gmail.com>
2018-06-13 22:31:13 +0700

    adf

--- source/backend/views/modules/view.php
+++ source/backend/views/modules/view.php
@@ -423,16 +423,16 @@ $outputMode = $model->outputModes;
         </div>
         <div class="buttom-content">
             <p>
-                <span>Lượng nước đã làm nóng <b><?php echo number_format($addParams->luong_nuoc_da_lam_nong); ?></b> m3</span>
+                <span><?php echo Yii::t('backend', 'The water is heated'); ?> <b><?php echo number_format($addParams->luong_nuoc_da_lam_nong); ?></b> m3</span>
             </p>
             <p>
-                <span>Lượng điện tiêu thụ <b><?php echo number_format($addParams->luong_dien_tieu_thu); ?></b> Kwh</span>
+                <span><?php echo Yii::t('backend', 'Power Consumption'); ?> <b><?php echo number_format($addParams->luong_dien_tieu_thu); ?></b> Kwh</span>
             </p>
             <p>
-                <span>Số tiền tiết kiệm <b><?php echo number_format($addParams->so_tien_tiet_kiem); ?></b> triệu</span>
+                <span><?php echo Yii::t('backend', 'Money of Saveings'); ?> <b><?php echo number_format($addParams->so_tien_tiet_kiem); ?></b> triệu</span>
             </p>
             <p>
-                <span>Lượng khí thải CO2 giảm <b><?php echo number_format($addParams->luong_khi_thai_co2_giam); ?></b> tấn</span>
+                <span><?php echo Yii::t('backend', 'CO2 emissions decrease'); ?> <b><?php echo number_format($addParams->luong_khi_thai_co2_giam); ?></b> tấn</span>
             </p>      
         </div>
     </div>      
