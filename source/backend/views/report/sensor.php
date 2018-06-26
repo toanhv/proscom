d61247aabb44d03e1df27540e4216d3f0290608c
toanhv <toanhv.itvn@gmail.com>
2018-06-26 22:11:19 +0700

    sdf

--- source/backend/views/report/sensor.php
+++ source/backend/views/report/sensor.php
@@ -12,11 +12,11 @@ $this->title = 'Sensor Report';
         <form method="post" action="/report/index" id="report-sensor-alarm">
             <div class="params">
                 <h3 class="title">ID: <?php echo $module->getModuleId() . ' - ' . Html::encode($module->name); ?></h3>
-                <input type="hidden" name="_csrf" value="<?php Yii::$app->request->csrfToken ?>">
+                <input type="hidden" name="_csrf" value="<?php Yii::$app->request->csrfToken ?>" autocomplete="off"/>
                 From:
                 <input type="text" name="from" id="report_from" value="<?php echo $from ?>">
                 To:
-                <input type="text" name="to" id="report_to" value="<?php echo $to ?>">
+                <input type="text" name="to" id="report_to" value="<?php echo $to ?>" autocomplete="off"/>
 
                 <button type="submit" name="export" value="0" class="btn-reprt btn-primary">Report</button>
                 <button type="submit" name="export" value="1" class="btn-reprt btn-primary">Excel</button>
