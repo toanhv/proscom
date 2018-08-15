553dc8b4e02e8699102ddb80b23c4091d65f990a
toanhv <toanhv.itvn@gmail.com>
2018-08-15 23:21:59 +0700

    ds

---
+++ source/mysql_backup.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+date=`/bin/date +%Y%m%d-%H%M`
+pass="OGE5ZTNl@#*#0818"
+mysqldump -uroot -p$pass proscom > /root/tool/proscom.sql
+cd /root/tool
+/bin/tar czf proscom-$date.tgz proscom.sql
+/bin/rm -rf proscom.sql
+/bin/mv proscom_hcatv-$date.tgz /root/backup/mysql/
+cd /root/backup/mysql
+for file1 in "$( /usr/bin/find /root/backup/mysql/ -type f -mtime +10 )"
+do
+   /bin/rm -rf $file1
+done
+
+exit 0
+~            
