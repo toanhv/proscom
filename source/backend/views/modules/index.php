de5d45fc9174440c3b3c1b8ebc45d4851e747552
toanhv <toanhv.itvn@gmail.com>
2018-08-16 22:53:58 +0700

    adf

--- source/backend/views/modules/index.php
+++ source/backend/views/modules/index.php
@@ -159,7 +159,7 @@ yii\widgets\Pjax::end();
 <?php
 $script = <<< JS
     $(document).ready(function () {
-        setTimeout(page_reload(0, '/'), 30000);
+        page_reload(0, '/');
     });
 JS;
 $this->registerJs($script);
