b49e8d12af9a8221bb4c0005cf15413a2760818f
toanhv9 <toanhv.itvn@gmail.com>
2019-04-08 22:35:38 +0700

    cache

--- source/console/controllers/ModuleController.php
+++ source/console/controllers/ModuleController.php
@@ -11,4 +11,19 @@ class ModuleController extends Controller {
         return \console\models\Modules::status();
     }
 
+    public function actionCached() {
+        $cache = \Yii::$app->cache;
+        $modules = \common\models\ModulesBase::findAll();
+        foreach ($modules as $item) {
+            //sensor
+            $key = 'getSensors_module_' . $item->id;
+            $cache->set($key, null);
+            $item->sensors;
+            //module id
+            $key = 'Module_id_' . $this->id;
+            $cache->set($key, null);
+            $item->getModuleId();
+        }
+    }
+
 }
